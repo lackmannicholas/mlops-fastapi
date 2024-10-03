10a1ea11351f127a3be0eed515b31f6eba22f27b
lackmannicholas <lackmannicholas@gmail.com>
2024-10-02 20:14:27 -0700

    adding initial text generation endpoint

---
+++ api/app.py
@@ -0,0 +1,23 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="gpt2")
+app = FastAPI()
+
+class Body(BaseModel):
+    text: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/generate")
+def generate_text(body: Body):
+    result = generator(body.text, max_length=35, num_return_sequences=1)
+    return result[0]
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
