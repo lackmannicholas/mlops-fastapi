273b9b7d717fd475596976495f8dc3f0246b9eee
Nick Lackman <lackmannicholas@gmail.com>
2024-11-04 12:01:49 -0800

    adding more click commands

--- cli/main.py
+++ cli/main.py
@@ -2,24 +2,44 @@
 
 """
 Main cli or app entry point
+
 """
 
 import click
 
+@click.group()
+def cli():
+    """Main entry point for the cli"""
+    pass
 
-@click.command("add")
+@cli.command("add")
 @click.argument("a", type=int)
 @click.argument("b", type=int)
 def add_cli(a, b):
     """Add two numbers together
 
     Example:
-        ./main.py add 1 2
+        cli add 1 2
+    """
+    
+    click.echo(click.style(str(a + b), fg="green"))
+
+def add_two(a: int):
+    """ Add two to a number """
+    return a + 2
+
+def add_three(a: int):
+    """ Add three to a number """
+    return a + 3
+
+#write a click cli method that takes in a number and adds 2 to it
+@cli.command("add_two")
+@click.argument("a", type=int)
+def add_two_cli(a):
+    """Add two to a number
+
+    Example:
+        cli add_two 1
     """
 
-    click.echo(click.style(str(a + b)), fg="green"))
-
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    click.echo(click.style(str(add_two(a)), fg="green"))
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='mlops-fastapi',
+  version='0.1.0',
+  packages=find_packages(),
+  py_modules=['main'],
+  install_requires=[
+    'fastapi',
+    'uvicorn',
+    'click'
+    # Add other dependencies here
+  ],
+  entry_points={
+    'console_scripts': [
+      'cli=cli.main:cli',  # Adjust 'main:main' to the appropriate entry point
+    ],
+  },
+)
